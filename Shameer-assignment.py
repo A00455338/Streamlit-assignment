03424e395c7f1f3c92daf3ee3bbf1ea3d037011c
A00455338 <navdeep.kaur.navdeep.kaur@smu.ca>
2022-04-20 21:58:23 -0300

    optimised the code

--- Shameer-assignment.py
+++ Shameer-assignment.py
@@ -29,32 +29,7 @@ else:
 df['date']=pd.to_datetime(df['date'],unit='ms')
 df1=df['date'].dt.strftime("%d-%b")
 
-df2=df['prices']
-
-if status=="usd":
-    df['prices'] = df['prices'].apply(lambda x: x/1.2613)
-elif status=="inr":
-    df['prices'] = df['prices'].apply(lambda x: x*60)
-
-
-df['date']=df['date'].apply(lambda x:(x-datetime(2022,1,1)).days)
-
-
-fig, ax = plt.subplots()
-plt.grid()
-ax.plot(df1,df2)
-ax.set_ylim(ymin=0)
-ax.set_xlim(xmax=Days_value)
-# Axis multiplier
-multiplier = 12
-
-# Set a tick on each integer multiple
-locator = plt.MultipleLocator(multiplier)
-
-# Set the locator of the major ticker
-ax.xaxis.set_major_locator(locator)
-
-st.pyplot(fig)
+st.line_chart(df['prices'])
 
 df3 = df['prices'].mean()
 str1= f'Average price during this time was {df3} in {status}'
